d86976038a565e6488c9f035cfc9eb1370941fcb
dajkagabi <dajkagabi01@gmail.com>
2025-08-03 15:54:51 +0200

    mission

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import video from "../../assets/hero.mp4";
 import logo from "../../assets/logo.png";
 import hero from "../../assets/hero.jpeg";
@@ -6,16 +6,24 @@ import hero from "../../assets/hero.jpeg";
 const Hero = () => {
   return (
     <section className="relative flex h-screen items-center justify-center">
-        <div className="absolute inset-0 -z-20 h-full w-full overflow-hidden">
-           <video src={video} className="h-full w-full object-cover" autoPlay loop muted playsInline poster={hero} ></video>
-        </div>
-        <div className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent from-70% to-black"></div>
-        <div className="relative z-20 flex h-screen flex-col justify-end pb-20">
-            <img src={logo} alt="logo" className="w-full p-4" />
-            <p className="p-4 text-lg tracking-tighter text-white">Paris</p>
-        </div>
-    </section> 
-  )
-}
+      <div className="absolute inset-0 -z-20 h-full w-full overflow-hidden">
+        <video
+          src={video}
+          className="h-full w-full object-cover"
+          autoPlay
+          loop
+          muted
+          playsInline
+          poster={hero}
+        ></video>
+      </div>
+      <div className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent from-70% to-black"></div>
+      <div className="relative z-20 flex h-screen flex-col justify-end pb-20">
+        <img src={logo} alt="logo" className="w-full p-4" />
+        <p className="p-4 text-lg tracking-tighter text-white">Paris</p>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
