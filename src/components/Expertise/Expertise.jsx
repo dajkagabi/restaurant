626b04ba1994a9de6ad6089cb7fa68096e350d7b
dajkagabi <dajkagabi01@gmail.com>
2025-08-03 16:27:37 +0200

    expertise

--- src/components/Expertise/Expertise.jsx
+++ src/components/Expertise/Expertise.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { CUSINES } from "../../constants";
 
 const Expertise = () => {
   return (
-    <div>Expertise</div>
-  )
-}
+    <section className="expertise">
+      <h2 className="my-8 text-center text-3xl tracking-tighter lg:text-4xl">
+        Our Expertise
+      </h2>
+      <div className="container mx-auto px-4">
+        {CUSINES.map((cusine, index) => (
+          <div
+            className="flex items-center border-b-4 border-dotted border-neutral-700/40 py-2"
+            key={index}>
+            <div className="flex-shrink-0 pr-8 text-2xl">{cusine.number}</div>
+            <div className="w-1/3 flex-shrink-0">
+              <img src={cusine.image} alt={cusine.title}
+              className="h-auto rounded-3xl" />
+            </div>
+            <div className="pl-8">
+              <h3 className="text-2xl uppercase tracking-tighter text-rose-300" >
+                {cusine.title}
+              </h3>
+              <p className="mt-4 text-lg tracking-tighter">{cusine.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Expertise
+export default Expertise;
